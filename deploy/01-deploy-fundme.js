d6d541090444c5e94b0654b8047a2a86c52e1c5d
Zhenyu Zhou <787599881@qq.com>
2025-08-03 20:14:05 +0800

    add unit test for fundme contract

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -18,18 +18,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
 
   let dataFeedAddr;
+  let confirmationBlocks;
   if (developmentChains.includes(network.name)) {
     const mockV3Aggregator = await deployments.get("MockV3Aggregator");
     dataFeedAddr = mockV3Aggregator.address;
+    confirmationBlocks = 0;
   } else {
     dataFeedAddr = networkConfig[network.config.chainId].ethUsdPriceFeed;
+    confirmationBlocks = CONFIRMATION_BLOCKS;
   }
 
   const fundMe = await deploy("FundMe", {
     from: owner,
     args: [LOCK_TIME, dataFeedAddr],
     log: true,
-    waitConfirmations: CONFIRMATION_BLOCKS,
+    waitConfirmations: confirmationBlocks,
   });
   //remove deployments directory or add --reset flag if you redeploy contract
 
